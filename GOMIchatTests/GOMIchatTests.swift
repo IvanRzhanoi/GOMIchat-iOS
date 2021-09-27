f6e74361b5d08e6dd68d58fbe77ea6b3ff2dc444
Ivan Rzhanoi <ivan.rzhanoi@gmail.com>
2021-09-27 15:19:47 +0300

    Initial Commit

---
+++ GOMIchatTests/GOMIchatTests.swift
@@ -0,0 +1,34 @@
+//
+//  GOMIchatTests.swift
+//  GOMIchatTests
+//
+//  Created by Ivan Rzhanoi on 27.9.2021.
+//  Copyright © 2021 Ivan Rzhanoi. All rights reserved.
+//
+
+import XCTest
+@testable import GOMIchat
+
+class GOMIchatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
